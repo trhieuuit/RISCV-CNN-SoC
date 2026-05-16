874ade4184fdd0d508eae6cc31dcebe0487af016
Trong Hieu <23520481@gm.uit.edu.vn>
2026-05-16 23:58:54 +0700

    optimize frequency

--- rtl/id_ex_reg.v
+++ rtl/id_ex_reg.v
@@ -83,7 +83,7 @@ module id_ex_reg(
 );
     
 always @(posedge clk_i) begin
-    if (!rst_ni || flush_i) begin
+    if (!rst_ni) begin
         pc_o          <= 32'b0;
         rd_o          <= 5'b0;
         rs1_o         <= 32'b0;
@@ -105,6 +105,28 @@ always @(posedge clk_i) begin
         we_regfile_o  <= 1'b0;
         cpu_halted_o  <= 1'b0;
         
+    end else if (flush_i) begin
+        we_regfile_o  <= 1'b0;
+        we_dmem_o     <= 1'b0;
+        d_dmemsel_o   <= 1'b0;
+        op_bl_o       <= 3'b010; 
+        is_jalr_o     <= 1'b0;
+        cpu_halted_o  <= 1'b0;
+        
+        pc_o          <= pc_i;
+        rd_o          <= rd_i;
+        rs1_o         <= rs1_i;
+        rs2_o         <= rs2_i;
+        rs1_addr_o    <= rs1_addr_i;
+        rs2_addr_o    <= rs2_addr_i;
+        imm_o         <= imm_i;
+        d1_alusel_o   <= d1_alusel_i;
+        d2_alusel_o   <= d2_alusel_i;
+        d1_bjsel_o    <= d1_bjsel_i;
+        d2_bjsel_o    <= d2_bjsel_i;
+        op_alu_o      <= op_alu_i;
+        rw_dmem_o     <= rw_dmem_i;
+        d_wbsel_o     <= d_wbsel_i;   
     end else begin
         pc_o          <= pc_i;
         rd_o          <= rd_i;
